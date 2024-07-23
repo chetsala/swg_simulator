09f9bfb243b26174e80479566755c75910e10fba
chetsala <chetsala@gmail.com>
2024-07-23 17:49:32 -0400

    Update swg_simulator.vhd
    Added in control system

--- swg_simulator.vhd
+++ swg_simulator.vhd
@@ -1,95 +1,203 @@
--- Entity Declaration
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/23/2024 04:54:03 PM
+-- Design Name: 
+-- Module Name: SWG_SYS - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity saltwater_chlorine_generator is
-    Port (
-        clk : in STD_LOGIC; -- Clock signal
-        rst : in STD_LOGIC; -- Reset signal
-        rotary_a : in STD_LOGIC; -- Rotary encoder signal A
-        rotary_b : in STD_LOGIC; -- Rotary encoder signal B
-        btn_add_salt : in STD_LOGIC; -- Button to add salt (Y16)
-        
-        sw_user_mode : in STD_LOGIC; -- Switch for user input mode (T16)
-        -- switch is T16. LED is D18. I misread the label on the board //GZM
-        btn_increase_temp : in STD_LOGIC; -- Button to increase temperature (P15)
-        -- switch is P15. LED is M15. I misread the label on the board //GZM
-        btn_increase_salt : in STD_LOGIC; -- Button to increase salt PPM (G15)
-        -- switch is G15. LED is M14. I misread the label on the board //GZM
-        
-        btn_pump_control : in STD_LOGIC; -- Button to control pool pump (K19)
-        -- Outputs to LEDs and displays
-        led_flow_status : out STD_LOGIC_VECTOR (2 downto 0); -- RGB LED for water flow (LD5)
-        led_system_status : out STD_LOGIC_VECTOR (2 downto 0); -- RGB LED for system status (LD6)
-        
-        ssd_display : out STD_LOGIC_VECTOR (6 downto 0); -- Seven-segment display
-        -- change vector from 7 downto 0 to 6 downto 0. reference assignment 03 //GZM
-        
-        oled_data : out STD_LOGIC_VECTOR (23 downto 0) -- OLED display data
-    );
-end saltwater_chlorine_generator;
+entity SWG_SYS is
+    Port ( CLK      : in  STD_LOGIC;
+           SW       : in  STD_LOGIC_VECTOR (3 downto 0);
+           BTN      : in  STD_LOGIC_VECTOR (4 downto 0);
+           LED5     : out STD_LOGIC_VECTOR (2 downto 0);
+           LED6     : out STD_LOGIC_VECTOR (2 downto 0);
+           ssd_display  : out STD_LOGIC_VECTOR (6 downto 0);
+           c : out STD_LOGIC);
+end SWG_SYS;
 
--- Architecture Body
-architecture Behavioral of saltwater_chlorine_generator is
-    signal pool_size_gallons : integer := 0; -- Pool size in thousands of gallons
-    signal salt_ppm : integer := 0; -- Salt concentration in parts per million (ppm)
-    signal temperature : integer := 0; -- Water temperature
-    signal water_flow_on : STD_LOGIC := '0'; -- Water flow status
-    signal system_status : STD_LOGIC_VECTOR (2 downto 0) := "000"; -- System status (green initially)
+architecture Behavioral of SWG_SYS is
+-- Constants
+    constant SALT_MIN : integer := 5;
+    constant SALT_MAX : integer := 12;
+    constant TEMP_MIN : integer := 7;
+
+    -- Types
+    type state_type is (INIT, SET_POOL_SIZE, SET_TEMP, SET_SALT, NORMAL_OP);
+    type display_type is (POOL_SIZE, TEMPERATURE, SALT, NORMAL);
+
+    -- Signals
+    signal current_state : state_type := INIT;
+    signal display_state : display_type := POOL_SIZE;
+    signal pool_gal : integer range 0 to 15 := 0;
+    signal temp : integer range 0 to 15 := 8;
+    signal salt_level : integer range 0 to 15 := 0;
+    signal pump_on : boolean := false;
+    signal system_good : boolean := false;
+    
+    -- 7-segment display signals
+    signal ssd_value : integer range 0 to 99 := 0;
+    signal ssd_refresh_counter : unsigned(19 downto 0) := (others => '0');
+    signal ssd_activedigit : std_logic_vector(1 downto 0) := (others => '0');
+
+    -- Button debounce signals
+    signal btn_debounce_counter : unsigned(19 downto 0) := (others => '0');
+    signal debounced_buttons : std_logic_vector(4 downto 0) := (others => '0');
+
+    -- New signals for seven segment display
+    signal tens_digit, ones_digit : integer range 0 to 9;
+    signal count : unsigned(16 - 1 downto 0); -- alternating between digits
+    signal c_temp : std_logic; -- temp var since "out" ports cannot be read in
+
 begin
-
-    -- Initialization Process
-    process(clk, rst)
+    -- Main process
+    process(CLK)
     begin
-        if rst = '1' then
-            pool_size_gallons <= 0;
-            salt_ppm <= 0;
-            temperature <= 0;
-            water_flow_on <= '0';
-            system_status <= "000"; -- Green initially
-        elsif rising_edge(clk) then
-            -- Initialization code for Pmods, LEDs, and other peripherals
-            -- Placeholder for actual initialization
-        end if;
-    end process;
-
-    -- User Input Handling Process
-    process(clk, rst)
-    begin
-        if rst = '1' then
-            -- Reset user input values
-            pool_size_gallons <= 0;
-            salt_ppm <= 0;
-            temperature <= 0;
-        elsif rising_edge(clk) then
-            -- Handle user input for initial settings
-            if sw_user_mode = '1' then
-                -- User input mode
-                if rotary_a = '1' and rotary_b = '0' then
-                    -- Handle rotary encoder input for navigating menus
-                elsif rotary_a = '0' and rotary_b = '1' then
-                    -- Handle rotary encoder input for changing values
-                end if;
-                
-                if btn_increase_salt = '1' then
-                    -- Change salt PPM
-                    salt_ppm <= salt_ppm + 500; -- Increment by 500 ppm
-                elsif btn_increase_temp = '1' then
-                    -- Change temperature
-                    temperature <= temperature + 1; -- Increment temperature by 1 degree
-                end if;
-                
-                if btn_add_salt = '1' then
-                    -- Add salt
-                    salt_ppm <= salt_ppm + 20;
-                    -- Trigger remeasurement logic
-                end if;
-            else
-                -- Normal running mode
+        if rising_edge(CLK) then
+            -- Button debounce
+            btn_debounce_counter <= btn_debounce_counter + 1;
+            if btn_debounce_counter = 0 then
+                debounced_buttons <= BTN;
             end if;
+
+            -- State machine
+            case current_state is
+                when INIT =>
+                    current_state <= SET_POOL_SIZE;
+                    display_state <= POOL_SIZE;
+                
+                when SET_POOL_SIZE =>
+                    if debounced_buttons(4) = '1' then  -- BTN4 pressed
+                        pool_gal <= to_integer(unsigned(SW));
+                        current_state <= SET_TEMP;
+                        display_state <= TEMPERATURE;
+                    end if;
+                
+                when SET_TEMP =>
+                    if debounced_buttons(4) = '1' then  -- BTN4 pressed
+                        temp <= to_integer(unsigned(SW));
+                        current_state <= SET_SALT;
+                        display_state <= SALT;
+                    end if;
+                
+                when SET_SALT =>
+                    if debounced_buttons(4) = '1' then  -- BTN4 pressed
+                        salt_level <= to_integer(unsigned(SW));
+                        current_state <= NORMAL_OP;
+                        display_state <= NORMAL;
+                    end if;
+                
+                when NORMAL_OP =>
+                    -- Handle pump on/off
+                    if debounced_buttons(2) = '1' then  -- BTN2 pressed
+                        pump_on <= not pump_on;
+                    end if;
+                    
+                    -- Handle add salt
+                    if debounced_buttons(3) = '1' then  -- BTN3 pressed
+                        if salt_level < 15 then
+                            salt_level <= salt_level + 1;
+                        end if;
+                    end if;
+                    
+                    -- Handle temperature adjustment
+                    if debounced_buttons(0) = '1' and temp < 15 then  -- BTN0 pressed
+                        temp <= temp + 1;
+                    elsif debounced_buttons(1) = '1' and temp > 0 then  -- BTN1 pressed
+                        temp <= temp - 1;
+                    end if;
+                    
+                    -- Check system status
+                    if temp > TEMP_MIN and salt_level >= SALT_MIN and salt_level <= SALT_MAX then
+                        system_good <= true;
+                    else
+                        system_good <= false;
+                    end if;
+            end case;
+
+            -- Update display value
+            case display_state is
+                when POOL_SIZE => ssd_value <= pool_gal;
+                when TEMPERATURE => ssd_value <= temp;
+                when SALT => ssd_value <= salt_level;
+                when NORMAL => ssd_value <= to_integer(unsigned(SW));
+            end case;
+
+            -- Update LEDs
+            if pump_on then
+                LED5 <= "010";  -- Green
+            else
+                LED5 <= "100";  -- Red
+            end if;
+
+            if current_state /= NORMAL_OP then
+                LED6 <= "110";  -- Yellow
+            elsif system_good then
+                LED6 <= "010";  -- Green
+            else
+                LED6 <= "100";  -- Red
+            end if;
+
+            -- Update 7-segment display digits
+            count <= count + 1;
+            tens_digit <= ssd_value / 10;
+            ones_digit <= ssd_value mod 10;
+            c_temp <= count(count'high);
         end if;
     end process;
+
+    -- 7-segment display digit multiplexing
+    process(c_temp, tens_digit, ones_digit)
+    begin
+        case c_temp is
+            when '0' =>
+                case ones_digit is
+                    when 0 => ssd_display <= "0111111";
+                    when 1 => ssd_display <= "0000110";
+                    when 2 => ssd_display <= "1011011";
+                    when 3 => ssd_display <= "1001111";
+                    when 4 => ssd_display <= "1100110";
+                    when 5 => ssd_display <= "1101101";
+                    when 6 => ssd_display <= "1111101";
+                    when 7 => ssd_display <= "0000111";
+                    when 8 => ssd_display <= "1111111";
+                    when 9 => ssd_display <= "1100111";
+                    when others => ssd_display <= "0000000";  -- Default case
+                end case;
+            when others =>
+                case tens_digit is
+                    when 0 => ssd_display <= "0111111";
+                    when 1 => ssd_display <= "0000110";
+                    when 2 => ssd_display <= "1011011";
+                    when 3 => ssd_display <= "1001111";
+                    when 4 => ssd_display <= "1100110";
+                    when 5 => ssd_display <= "1101101";
+                    when 6 => ssd_display <= "1111101";
+                    when 7 => ssd_display <= "0000111";
+                    when 8 => ssd_display <= "1111111";
+                    when 9 => ssd_display <= "1100111";
+                    when others => ssd_display <= "0000000";  -- Default case
+                end case;
+        end case;
+    end process;
+
+    -- Active digit selection
+    c <= c_temp;
 
 end Behavioral;
