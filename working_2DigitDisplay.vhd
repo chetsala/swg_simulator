61dc49f21b95825fbaf07aacac1e5d4c715a973c
tawreosir <tawreosir@outlook.com>
2024-07-24 19:05:40 -0400

    7 seg added button debounce logic for add/subtract

--- working_2DigitDisplay.vhd
+++ working_2DigitDisplay.vhd
@@ -18,7 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -27,78 +26,146 @@ entity sevenSeg is
   port (
     clk : in std_logic;  -- Clock input
     reset : in std_logic;  -- Reset input
-    value : in STD_LOGIC_VECTOR (3 downto 0); -- used to test w/switches, replace 'value' in process (clk, reset)
---    value : in integer range 0 to 99;  -- Value to be displayed (0-99)
-    ssd_display : out std_logic_vector(6 downto 0);  -- 7 segment display output
-    c : out std_logic  -- digit select signal
+    add : in std_logic;  -- Add input
+    subtract : in std_logic;  -- Subtract input
+    sw : in std_logic_vector(3 downto 0);  -- Switch input for testing
+    ssd_display : out std_logic_vector(6 downto 0);  -- 7-segment display output
+    c : out std_logic  -- Digit select signal
   );
 end sevenSeg;
 
 architecture behavioral of sevenSeg is
 
     signal tens_digit, ones_digit : integer range 0 to 9;
-    signal count : unsigned(16 - 1 downto 0); -- alternating between digits
-    signal c_temp : std_logic; -- temp var since "out" ports cannot be read in
+    signal value, num : integer range 0 to 99 := 0;
+    signal count : unsigned(15 downto 0) := (others => '0');  -- Alternating between digits
+    signal c_temp : std_logic;  -- Temp variable since "out" ports cannot be read in
+    
+    -- Debouncing signals and parameters
+    constant DEBOUNCE_MAX : integer := 100000;  -- Adjust debounce time as needed
+    signal add_debounce_counter, subtract_debounce_counter : integer := 0;
+    signal add_stable, subtract_stable : std_logic := '0';
+    signal debounce_flag : std_logic := '0';
 
 begin
-
-    process (clk, reset)
-        begin
+    -- Debouncing process for "add" button
+    process(clk, reset)
+    begin
         if reset = '1' then
+            add_debounce_counter <= 0;
+            add_stable <= '0';
+        elsif rising_edge(clk) then
+            if add = '1' then
+                if add_debounce_counter < DEBOUNCE_MAX then
+                    add_debounce_counter <= add_debounce_counter + 1;
+                else
+                    add_stable <= '1';
+                end if;
+            else
+                add_debounce_counter <= 0;
+                add_stable <= '0';
+            end if;
+        end if;
+    end process;
+
+    -- Debouncing process for "subtract" button
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            subtract_debounce_counter <= 0;
+            subtract_stable <= '0';
+        elsif rising_edge(clk) then
+            if subtract = '1' then
+                if subtract_debounce_counter < DEBOUNCE_MAX then
+                    subtract_debounce_counter <= subtract_debounce_counter + 1;
+                else
+                    subtract_stable <= '1';
+                end if;
+            else
+                subtract_debounce_counter <= 0;
+                subtract_stable <= '0';
+            end if;
+        end if;
+    end process;
+
+    -- Main process to handle add/subtract logic and update 7-segment display
+    process(clk, reset)
+    begin
+        if reset = '1' then
+            num <= 0;
             count <= (others => '0');
             c_temp <= '0';
             tens_digit <= 0;
             ones_digit <= 0;
         elsif rising_edge(clk) then
-            count <= count + 1;
-
+            -- Add/Subtract logic
+            if add_stable = '1' and debounce_flag = '0' and num < 99 then
+                num <= num + 1;
+                debounce_flag <= '1';
+            elsif subtract_stable = '1' and debounce_flag = '0' and num > 0 then
+                num <= num - 1;
+                debounce_flag <= '1';
+            elsif add_stable = '0' and subtract_stable = '0' then
+                debounce_flag <= '0';
+            end if;
+            
+            -- Update 7-segment display value based on switches
+            if sw = "0001" then
+                value <= 99;
+            elsif sw = "0010" then
+                value <= 21;
+            elsif sw = "0100" then
+                value <= num;
+            else
+                value <= to_integer(unsigned(sw));
+            end if;
+            
             -- Convert to BCD
-      tens_digit <= TO_INTEGER(unsigned(value)) / 10; -- use when value == switches
-      ones_digit <= TO_INTEGER(unsigned(value)) mod 10; -- use when value == switches
-      --tens_digit <= value / 10; -- use when value == integer
-      --ones_digit <= value mod 10; -- use when value == integer
-
+            tens_digit <= value / 10;
+            ones_digit <= value mod 10;
+            
             -- Alternate digit selection
+            count <= count + 1;
             c_temp <= count(count'high);
         end if;
     end process;
 
-  process (c_temp, tens_digit, ones_digit)
-  begin
-    -- Select digit based on c_temp
-    case c_temp is
-      when '0' =>
-        case ones_digit is
-          when 0 => ssd_display <= "0111111";
-          when 1 => ssd_display <= "0000110";
-          when 2 => ssd_display <= "1011011";
-          when 3 => ssd_display <= "1001111";
-          when 4 => ssd_display <= "1100110";
-          when 5 => ssd_display <= "1101101";
-          when 6 => ssd_display <= "1111101";
-          when 7 => ssd_display <= "0000111";
-          when 8 => ssd_display <= "1111111";
-          when 9 => ssd_display <= "1101111";
-          when others => ssd_display <= "0000000";  -- Default case
+    -- 7-segment display logic
+    process(c_temp, tens_digit, ones_digit)
+    begin
+        case c_temp is
+            when '0' =>
+                case ones_digit is
+                    when 0 => ssd_display <= "0111111";
+                    when 1 => ssd_display <= "0000110";
+                    when 2 => ssd_display <= "1011011";
+                    when 3 => ssd_display <= "1001111";
+                    when 4 => ssd_display <= "1100110";
+                    when 5 => ssd_display <= "1101101";
+                    when 6 => ssd_display <= "1111101";
+                    when 7 => ssd_display <= "0000111";
+                    when 8 => ssd_display <= "1111111";
+                    when 9 => ssd_display <= "1101111";
+                    when others => ssd_display <= "0000000";  -- Default case
+                end case;
+            when others =>
+                case tens_digit is
+                    when 0 => ssd_display <= "0111111";
+                    when 1 => ssd_display <= "0000110";
+                    when 2 => ssd_display <= "1011011";
+                    when 3 => ssd_display <= "1001111";
+                    when 4 => ssd_display <= "1100110";
+                    when 5 => ssd_display <= "1101101";
+                    when 6 => ssd_display <= "1111101";
+                    when 7 => ssd_display <= "0000111";
+                    when 8 => ssd_display <= "1111111";
+                    when 9 => ssd_display <= "1101111";
+                    when others => ssd_display <= "0000000";  -- Default case
+                end case;
         end case;
-      when others =>
-        case tens_digit is
-          when 0 => ssd_display <= "0111111";
-          when 1 => ssd_display <= "0000110";
-          when 2 => ssd_display <= "1011011";
-          when 3 => ssd_display <= "1001111";
-          when 4 => ssd_display <= "1100110";
-          when 5 => ssd_display <= "1101101";
-          when 6 => ssd_display <= "1111101";
-          when 7 => ssd_display <= "0000111";
-          when 8 => ssd_display <= "1111111";
-          when 9 => ssd_display <= "1101111";
-          when others => ssd_display <= "0000000";  -- Default case
-        end case;
-    end case;
-  end process;
+    end process;
 
-  -- Assign the common cathode/anode control signal
-  c <= c_temp;
+    -- Assign the common cathode/anode control signal
+    c <= c_temp;
 
 end architecture;
--- working_2DigitDisplay.xdc
+++ working_2DigitDisplay.xdc
@@ -10,17 +10,17 @@ set_property -dict { PACKAGE_PIN K17   IOSTANDARD LVCMOS33 } [get_ports { clk }]
 
 
 ##Switches
-set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { value[0] }]; #IO_L19N_T3_VREF_35 Sch=sw[0]
-set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { value[1] }]; #IO_L24P_T3_34 Sch=sw[1]
-set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports { value[2] }]; #IO_L4N_T0_34 Sch=sw[2]
-set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { value[3] }]; #IO_L9P_T1_DQS_34 Sch=sw[3]
+set_property -dict { PACKAGE_PIN G15   IOSTANDARD LVCMOS33 } [get_ports { sw[0] }]; #IO_L19N_T3_VREF_35 Sch=sw[0]
+set_property -dict { PACKAGE_PIN P15   IOSTANDARD LVCMOS33 } [get_ports { sw[1] }]; #IO_L24P_T3_34 Sch=sw[1]
+set_property -dict { PACKAGE_PIN W13   IOSTANDARD LVCMOS33 } [get_ports { sw[2] }]; #IO_L4N_T0_34 Sch=sw[2]
+set_property -dict { PACKAGE_PIN T16   IOSTANDARD LVCMOS33 } [get_ports { sw[3] }]; #IO_L9P_T1_DQS_34 Sch=sw[3]
 
 
 ##Buttons
 set_property -dict { PACKAGE_PIN K18   IOSTANDARD LVCMOS33 } [get_ports { reset }]; #IO_L12N_T1_MRCC_35 Sch=btn[0]
 #set_property -dict { PACKAGE_PIN P16   IOSTANDARD LVCMOS33 } [get_ports { btn[1] }]; #IO_L24N_T3_34 Sch=btn[1]
-#set_property -dict { PACKAGE_PIN K19   IOSTANDARD LVCMOS33 } [get_ports { btn[2] }]; #IO_L10P_T1_AD11P_35 Sch=btn[2]
-#set_property -dict { PACKAGE_PIN Y16   IOSTANDARD LVCMOS33 } [get_ports { btn[3] }]; #IO_L7P_T1_34 Sch=btn[3]
+set_property -dict { PACKAGE_PIN K19   IOSTANDARD LVCMOS33 } [get_ports { add }]; #IO_L10P_T1_AD11P_35 Sch=btn[2]
+set_property -dict { PACKAGE_PIN Y16   IOSTANDARD LVCMOS33 } [get_ports { subtract }]; #IO_L7P_T1_34 Sch=btn[3]
 
 
 ##LEDs
@@ -123,10 +123,10 @@ set_property -dict { PACKAGE_PIN K18   IOSTANDARD LVCMOS33 } [get_ports { reset 
                                                                                                                                  
                                                                                                                                  
 ##Pmod Header JC                                                                                                                  
-#set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33     } [get_ports { jc[0] }]; #IO_L10P_T1_34 Sch=jc_p[1]   			 
-#set_property -dict { PACKAGE_PIN W15   IOSTANDARD LVCMOS33     } [get_ports { jc[1] }]; #IO_L10N_T1_34 Sch=jc_n[1]		     
-#set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33     } [get_ports { jc[2] }]; #IO_L1P_T0_34 Sch=jc_p[2]              
-#set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33     } [get_ports { jc[3] }]; #IO_L1N_T0_34 Sch=jc_n[2]              
+set_property -dict { PACKAGE_PIN V15   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[0] }]; #IO_L10P_T1_34 Sch=jc_p[1]   			 
+set_property -dict { PACKAGE_PIN W15   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[1] }]; #IO_L10N_T1_34 Sch=jc_n[1]		     
+set_property -dict { PACKAGE_PIN T11   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[2] }]; #IO_L1P_T0_34 Sch=jc_p[2]              
+set_property -dict { PACKAGE_PIN T10   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[3] }]; #IO_L1N_T0_34 Sch=jc_n[2]              
 #set_property -dict { PACKAGE_PIN W14   IOSTANDARD LVCMOS33     } [get_ports { jc[4] }]; #IO_L8P_T1_34 Sch=jc_p[3]              
 #set_property -dict { PACKAGE_PIN Y14   IOSTANDARD LVCMOS33     } [get_ports { jc[5] }]; #IO_L8N_T1_34 Sch=jc_n[3]              
 #set_property -dict { PACKAGE_PIN T12   IOSTANDARD LVCMOS33     } [get_ports { jc[6] }]; #IO_L2P_T0_34 Sch=jc_p[4]              
@@ -134,10 +134,10 @@ set_property -dict { PACKAGE_PIN K18   IOSTANDARD LVCMOS33 } [get_ports { reset 
                                                                                                                                  
                                                                                                                                  
 ##Pmod Header JD                                                                                                                  
-set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[0] }]; #IO_L5P_T0_34 Sch=jd_p[1]                  
-set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[1] }]; #IO_L5N_T0_34 Sch=jd_n[1]				 
-set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[2] }]; #IO_L6P_T0_34 Sch=jd_p[2]                  
-set_property -dict { PACKAGE_PIN R14   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[3] }]; #IO_L6N_T0_VREF_34 Sch=jd_n[2]             
+set_property -dict { PACKAGE_PIN T14   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[4] }]; #IO_L5P_T0_34 Sch=jd_p[1]                  
+set_property -dict { PACKAGE_PIN T15   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[5] }]; #IO_L5N_T0_34 Sch=jd_n[1]				 
+set_property -dict { PACKAGE_PIN P14   IOSTANDARD LVCMOS33     } [get_ports { ssd_display[6] }]; #IO_L6P_T0_34 Sch=jd_p[2]                  
+set_property -dict { PACKAGE_PIN R14   IOSTANDARD LVCMOS33     } [get_ports { c }]; #IO_L6N_T0_VREF_34 Sch=jd_n[2]             
 #set_property -dict { PACKAGE_PIN U14   IOSTANDARD LVCMOS33     } [get_ports { SevenSD[0] }]; #IO_L11P_T1_SRCC_34 Sch=jd_p[3]            
 #set_property -dict { PACKAGE_PIN U15   IOSTANDARD LVCMOS33     } [get_ports { SevenSD[1] }]; #IO_L11N_T1_SRCC_34 Sch=jd_n[3]            
 #set_property -dict { PACKAGE_PIN V17   IOSTANDARD LVCMOS33     } [get_ports { SevenSD[2] }]; #IO_L21P_T3_DQS_34 Sch=jd_p[4]             
@@ -145,10 +145,10 @@ set_property -dict { PACKAGE_PIN R14   IOSTANDARD LVCMOS33     } [get_ports { ss
                                                                                                                                  
                                                                                                                                  
 ##Pmod Header JE                                                                                                                  
-set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[4] }]; #IO_L4P_T0_34 Sch=je[1]						 
-set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[5] }]; #IO_L18N_T2_34 Sch=je[2]                     
-set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[6] }]; #IO_25_35 Sch=je[3]                          
-set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { c }]; #IO_L19P_T3_35 Sch=je[4]                     
+#set_property -dict { PACKAGE_PIN V12   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[4] }]; #IO_L4P_T0_34 Sch=je[1]						 
+#set_property -dict { PACKAGE_PIN W16   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[5] }]; #IO_L18N_T2_34 Sch=je[2]                     
+#set_property -dict { PACKAGE_PIN J15   IOSTANDARD LVCMOS33 } [get_ports { ssd_display[6] }]; #IO_25_35 Sch=je[3]                          
+#set_property -dict { PACKAGE_PIN H15   IOSTANDARD LVCMOS33 } [get_ports { c }]; #IO_L19P_T3_35 Sch=je[4]                     
 #set_property -dict { PACKAGE_PIN V13   IOSTANDARD LVCMOS33 } [get_ports { SevenSD[4] }]; #IO_L3N_T0_DQS_34 Sch=je[7]                  
 #set_property -dict { PACKAGE_PIN U17   IOSTANDARD LVCMOS33 } [get_ports { SevenSD[5] }]; #IO_L9N_T1_DQS_34 Sch=je[8]                  
 #set_property -dict { PACKAGE_PIN T17   IOSTANDARD LVCMOS33 } [get_ports { SevenSD[6] }]; #IO_L20P_T3_34 Sch=je[9]                     
